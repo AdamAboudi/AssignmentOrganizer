cb40549b8b49922924123c44da16488d4e50c028
Adam Aboudi <adamaboudi123@gmail.com>
2018-07-24 13:24:32 -0400

    Added files for JavaFX

---
+++ AssignmentOrganizer/src/main/Controller.java
@@ -0,0 +1,11 @@
+package main;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void buttonPressed(ActionEvent event){
+        System.out.println("Link Check.");
+    }
+
+}
--- AssignmentOrganizer/src/main/main.java
+++ AssignmentOrganizer/src/main/main.java
@@ -3,11 +3,29 @@ package main;
 import java.io.File;
 import java.io.IOException;
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class main extends Application{
+
+    public void start( Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
+        primaryStage.setTitle("Organizer");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+
+
+    }
     public static void main(String[] args) throws IOException {
+
+       // launch(args);
         File file = new File("assignments.json");
         Organizer main = new Organizer(file);
+
         main.run();
+
     }
 }
