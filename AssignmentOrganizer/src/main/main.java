be8a74dcb133f24df65a3b33f3c7cb5b10349748
Adam Aboudi <adamaboudi123@gmail.com>
2018-07-25 14:11:29 -0400

    changes to javafx

--- AssignmentOrganizer/src/main/main.java
+++ AssignmentOrganizer/src/main/main.java
@@ -7,6 +7,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
 
 public class main extends Application{
@@ -21,7 +23,7 @@ public class main extends Application{
     }
     public static void main(String[] args) throws IOException {
 
-       // launch(args);
+        //launch(args);
         File file = new File("assignments.json");
         Organizer main = new Organizer(file);
 
